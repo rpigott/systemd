rpigott/systemd

commit c83cd8da4916757c4a54bce4c269ccd3c6f49156

resolved: demote the global unicast scope
This will greatly reduce the number of cases where the global unicast
scope overlaps with link scopes configured as default-route, making it
feasible to use the global DNS setting in conjunction with per-link dns
servers configured by the network.

This change is preferred over demoting links to default-route=no where
the user prefers to use the network provided DNS servers, and I expect
it is non-disruptive in that it should not degrade the efficacy of any
existing configuration.

man/systemd-resolved.service.xml
@@ -217,7 +217,10 @@
 
       <listitem><para>If a query does not match any configured routing domain (either per-link or global), it
       is sent to all DNS servers that are configured on links with the <varname>DefaultRoute=</varname>
-      option set, as well as the globally configured DNS server.</para></listitem>
+      option set.</para></listitem>
+
+      <listitem><para>If no links are configured with <varname>DefaultRoute=</varname>, it is sent to the
+      globally configured DNS server.</para></listitem>
 
       <listitem><para>If there is no link configured as <varname>DefaultRoute=</varname> and no global DNS
       server configured, one of the compiled-in fallback DNS servers is used.</para></listitem>

src/resolve/resolved-dns-scope.c
@@ -778,7 +778,8 @@ DnsScopeMatch dns_scope_good_domain(
                 if (!dns_scope_is_default_route(s))
                         return DNS_SCOPE_NO;
 
-                return DNS_SCOPE_MAYBE;
+                /* Prefer suitable per-link scopes where possible */
+                return s->link ? DNS_SCOPE_MAYBE : DNS_SCOPE_LAST_RESORT;
         }
 
         case DNS_PROTOCOL_MDNS: {

commit ae7a0e4673a3d6e583f7c8a40a2faa168e6b615a

resolved: demote only the fallback servers in the global scope
This softens the behavior introduced in c83cd8da4916, and simultaneously
expands the role of fallback servers so they are applied not only when
there are no dns servers configured, but when all the configured dns
servers are configured only for non-default-route links.

The intent is that the global FallbackDNS (instead of DNS) can now be
used in conjunction with the per-link dns, providing a fallback behavior
without introducing a scope overlap.

Fixes: c83cd8da4916 (resolved: demote the global unicast scope)

man/systemd-resolved.service.xml
@@ -217,13 +217,11 @@
 
       <listitem><para>If a query does not match any configured routing domain (either per-link or global), it
       is sent to all DNS servers that are configured on links with the <varname>DefaultRoute=</varname>
-      option set.</para></listitem>
+      option set, as well as the globally configured DNS server.</para></listitem>
 
-      <listitem><para>If no links are configured with <varname>DefaultRoute=</varname>, it is sent to the
-      globally configured DNS server.</para></listitem>
-
-      <listitem><para>If there is no link configured as <varname>DefaultRoute=</varname> and no global DNS
-      server configured, one of the compiled-in fallback DNS servers is used.</para></listitem>
+      <listitem><para>If there are no DNS servers configured on any link also configured as
+      <varname>DefaultRoute=</varname> and no global DNS server configured, one of the compiled-in fallback
+      DNS servers is used.</para></listitem>
 
       <listitem><para>Otherwise the unicast DNS query fails, as no suitable DNS servers can be determined.
       </para></listitem>

src/resolve/resolved-dns-scope.c
@@ -718,8 +718,10 @@ DnsScopeMatch dns_scope_good_domain(
                 }
 
                 /* Never route things to scopes that lack DNS servers */
-                if (!dns_scope_get_dns_server(s))
+                if (!dns_scope_get_dns_server(s)) {
+                        log_info("no dns servers on %s\n", s->link ? s->link->ifname : "manager");
                         return DNS_SCOPE_NO;
+                }
 
                 /* Always honour search domains for routing queries, except if this scope lacks DNS servers. Note that
                  * we return DNS_SCOPE_YES here, rather than just DNS_SCOPE_MAYBE, which means other wildcard scopes
@@ -779,7 +781,10 @@ DnsScopeMatch dns_scope_good_domain(
                         return DNS_SCOPE_NO;
 
                 /* Prefer suitable per-link scopes where possible */
-                return s->link ? DNS_SCOPE_MAYBE : DNS_SCOPE_LAST_RESORT;
+                if (!s->link && s->manager->current_dns_server->type == DNS_SERVER_FALLBACK)
+                        return DNS_SCOPE_LAST_RESORT;
+
+                return DNS_SCOPE_MAYBE;
         }
 
         case DNS_PROTOCOL_MDNS: {

src/resolve/resolved-dns-server.c
@@ -909,7 +909,7 @@ DnsServer *manager_get_dns_server(Manager *m) {
                  * servers */
 
                 HASHMAP_FOREACH(l, m->links)
-                        if (l->dns_servers) {
+                        if (l->dns_servers && l->default_route) {
                                 found = true;
                                 break;
                         }